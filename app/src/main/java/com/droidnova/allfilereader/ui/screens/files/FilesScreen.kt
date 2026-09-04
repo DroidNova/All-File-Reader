8f7e40990ce910befd1d78b70e658ffb70047be6
Ashim khan <116441770+Ashim74@users.noreply.github.com>
2026-09-05 01:56:50 +0530

    Fix debug Kotlin compilation

--- app/src/main/java/com/droidnova/allfilereader/ui/screens/files/FilesScreen.kt
+++ app/src/main/java/com/droidnova/allfilereader/ui/screens/files/FilesScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.LifecycleResumeEffect
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
